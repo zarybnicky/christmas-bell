c4a1cb2b7525854f5e5638cea098932478739c0e
Jakub Zárybnický <kubaz@inuits.eu>
2018-12-24 01:27:21 +0100

    Ring A-Ling A-Ling

---
+++ index.js
@@ -0,0 +1,28 @@
+const play = require('audio-play');
+const load = require('audio-loader');
+const app = require('express')();
+
+let bell;
+load('./bell-ringing.wav').then(x => bell = x);
+function ring(cb, n = 1, timeout = 250) {
+  if (n > 0) {
+    play(bell, () => setTimeout(() => ring(cb, n - 1, timeout), timeout));
+  } else {
+    cb();
+  }
+}
+
+app.get('/', (req, res) => res.send(`
+<html>
+  <head><title>Christmas Bells</title></head>
+  <body>
+    <form method=post>
+      <button type=submit>Ring!</button>
+    </form>
+  </body>
+</html>
+`));
+
+app.post('/', (req, res) => ring(() => res.redirect('/'), 2, 200));
+
+app.listen(3000, () => console.log(`Listening on port 3000!`));
