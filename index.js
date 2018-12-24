6012a711c6427637619b69765f8ecd2d5463bb89
Jakub Zárybnický <kubaz@inuits.eu>
2018-12-24 12:06:39 +0100

    Bigger button

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = require('express')();
 
 let bell;
 load('./bell-ringing.wav').then(x => bell = x);
-function ring(cb, n = 1, timeout = 250) {
+function ring(cb, n = 1) {
   if (n > 0) {
-    play(bell, () => setTimeout(() => ring(cb, n - 1, timeout), timeout));
+    play(bell, () => setTimeout(() => ring(cb, n - 1), 200));
   } else {
     cb();
   }
@@ -17,12 +17,12 @@ app.get('/', (req, res) => res.send(`
   <head><title>Christmas Bells</title></head>
   <body>
     <form method=post>
-      <button type=submit>Ring!</button>
+      <button type=submit style="width:100%;height:10em;">Ring!</button>
     </form>
   </body>
 </html>
 `));
 
-app.post('/', (req, res) => ring(() => res.redirect('/'), 2, 200));
+app.post('/', (req, res) => ring(() => res.redirect('/'), 2));
 
 app.listen(3000, () => console.log(`Listening on port 3000!`));
